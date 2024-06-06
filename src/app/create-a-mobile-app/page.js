0d9439f84afc9ddfc60d74d8e3b4a796c7024d07
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-06-06 06:00:05 +0500

    new

--- src/app/create-a-mobile-app/page.js
+++ src/app/create-a-mobile-app/page.js
@@ -34,11 +34,21 @@ export default function gamedevelopmentcompany() {
     checkMobile();
     window.addEventListener("resize", checkMobile);
 
-    // Show mobile components immediately
-    setShowMobileComponents(true);
+    const handleTouchOrScroll = () => {
+      setShowMobileComponents(true);
+      // Remove event listeners after mobile components are shown
+      window.removeEventListener("scroll", handleTouchOrScroll);
+      window.removeEventListener("touchstart", handleTouchOrScroll);
+    };
+
+    // Add event listeners for touch and scroll events
+    window.addEventListener("scroll", handleTouchOrScroll);
+    window.addEventListener("touchstart", handleTouchOrScroll);
 
     return () => {
       window.removeEventListener("resize", checkMobile);
+      window.removeEventListener("scroll", handleTouchOrScroll);
+      window.removeEventListener("touchstart", handleTouchOrScroll);
     };
   }, []);
 
